fbfd54af40fb501a029da4e3e61719245f64f27d
Arif Wider <awider@thoughtworks.com>
2015-02-06 16:55:15 +0100

    most basic conversion example

--- implicits/src/main/scala/ImplicitsExamples.scala
+++ implicits/src/main/scala/ImplicitsExamples.scala
@@ -2,4 +2,16 @@ object ImplicitsExamples {
   def main(args: Array[String]): Unit = {
     println("Implicit tests")
   }
+
+  "hello".toAllUpper
+
+  class RichString(str: String) {
+    def toAllUpper = str.toUpperCase
+  }
+  implicit def string2RichString(str: String): RichString = new RichString(str)
+  //implicit def string2RichString(str: String): Object = new RichString(str)
+
+  string2RichString("hello").toAllUpper
+
+
 }
