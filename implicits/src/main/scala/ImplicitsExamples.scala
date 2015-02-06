d4cf4bbef48c91f36f6baddbfeb3ce4bf1f71be3
Arif Wider <awider@thoughtworks.com>
2015-02-06 17:05:03 +0100

    started with example for context bounds

--- implicits/src/main/scala/ImplicitsExamples.scala
+++ implicits/src/main/scala/ImplicitsExamples.scala
@@ -13,5 +13,24 @@ object ImplicitsExamples {
 
   string2RichString("hello").toAllUpper
 
+  //---------- Context bounds --------
+
+  implicit def ordered[A <: Ordered[A]]: Ordering[A] = new Ordering[A] {
+    def compare(x: A, y: A) = x.compare(y)
+  }
+
+  def printOrdered[A](input: A)(implicit ord: Ordered[A]) {
+    println(ord.compare(input))
+  }
+
+  def printOrdered2[A : Ordered](input: A) {
+  //def printOrdered2[A](input: A) {
+    printOrdered() //gets the ordering implicitly
+  }
+
+  printOrdered2(1 :: 2 :: Nil)
+
+
+
 
 }
