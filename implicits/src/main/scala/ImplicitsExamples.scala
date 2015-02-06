479fcef071fb88b57bb2a1463c61350587249dbf
Arif Wider <awider@thoughtworks.com>
2015-02-06 23:20:51 +0100

    nice little context bound example

--- implicits/src/main/scala/ImplicitsExamples.scala
+++ implicits/src/main/scala/ImplicitsExamples.scala
@@ -15,21 +15,24 @@ object ImplicitsExamples {
 
   //---------- Context bounds --------
 
-  implicit def ordered[A <: Ordered[A]]: Ordering[A] = new Ordering[A] {
-    def compare(x: A, y: A) = x.compare(y)
+  trait Comparator[A] { def isOrdered(a1: A, a2: A): Boolean }
+  implicit object IntComparator extends Comparator[Int] {
+    override def isOrdered(a1: Int, a2: Int): Boolean = if(a1 < a2) true else false
   }
 
-  def printOrdered[A](input: A)(implicit ord: Ordered[A]) {
-    println(ord.compare(input))
+  //def orderPairList[A](list: List[(A,A)])(implicit ord: Comparator[A]) = {
+  def orderPairList[A : Comparator](list: List[(A,A)]) = {
+    for { pair <- list } yield orderPair(pair)
   }
 
-  def printOrdered2[A : Ordered](input: A) {
-  //def printOrdered2[A](input: A) {
-    printOrdered() //gets the ordering implicitly
+  def orderPair[A](pair: (A, A))(implicit ord: Comparator[A]) = {
+    if(ord.isOrdered(pair._1, pair._2)) pair else pair.swap
   }
+//  def orderPair[A : Comparator](pair: (A, A)) = {
+//    if(implicitly[Comparator[A]].isOrdered(pair._1, pair._2)) pair else pair.swap
+//  }
 
-  printOrdered2(1 :: 2 :: Nil)
-
+  println(orderPairList((1,2) :: (2,1) :: Nil))
 
 
 
