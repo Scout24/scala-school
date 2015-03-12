cf58a8576dcea8181234559674112eff801f213f
Arif Wider <awider@thoughtworks.com>
2015-03-12 17:08:30 +0100

    tidied up currying examples

--- traits-and-currying/src/main/scala/CurryingExamples.scala
+++ traits-and-currying/src/main/scala/CurryingExamples.scala
@@ -1,19 +1,26 @@
 object CurryingExamples {
 
-  def fun(a: Int) = (b: Int) => (a,b)
-  def funGen[A,B](a: A) = (b: B) => (a,b)
-  def fun2(a: Int)(b: Int) = (a,b)
-  def fun2Gen[A,B](a: A)(b: B) = (a,b)
-  def fun3Gen[A](a: A)(b: Some[A]) = (a,b)
-  def fun4Gen[A](a: A, b: Some[A]) = (a,b)
+  type A = Int
+  type B = Option[Int]
+  type C = (Int, Option[Int])
+
+  type ABtoC = (A,B) => C
+  type AtoBtoC = A => (B => C)
+
+  def fun(a: A, b: B) = (a,b)
+  def funCurried(a: A) = (b: B) => (a,b)
+  def funCurried2(a: A)(b: B) = (a,b)
+
+  def funGen[T](a: T, b: Some[T]) = (a,b)
+  def funGenCurried[T](a: T)(b: Some[T]) = (a,b)
+
+  def modN(n: Int)(x: Int) = ((x % n) == 0)
+  def modNnc(n: Int, x: Int) = ((x % n) == 0)
+  def filterByMod2(l:Seq[Int]) = l.filter(modN(2))
+  def filterByMod2nc(l:Seq[Int]) = l.filter(modNnc(2,_))
 
   def main(args: Array[String]): Unit = {
-    println(fun(1)(2))
-    println(fun2(1)(2))
-    val f3g: (Some[Int]) => (Int, Some[Int]) = fun3Gen(1)
-    val f3g2: (Some[Int]) => (Int, Some[Int]) = fun3Gen(1)(_)
-    val f4g: (Some[Int]) => (Some[(Int, Some[Int])]) => ((Int, Some[Int]), Some[(Int, Some[Int])]) = fun3Gen(1, _:Some[Int])
-    //val y = fun3Gen(1)(Some("A")) // correctly shows a compile error
-    val x: (Any, Some[Any]) = fun4Gen(1, Some("A")) //type inference cannot work here
+    val x = funGen(1, Some("A")) //type inference cannot help here
+    //val y = funGenCurried(1)(Some("A")) // correctly shows a compile error
   }
 }
