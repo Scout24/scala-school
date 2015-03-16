ae34e0630a28bd237df6f95bd2924f580d78ef31
Arif Wider <awider@thoughtworks.com>
2015-03-16 17:38:09 +0100

    currying exercise

--- traits-and-currying/src/main/scala/CurryingExamples.scala
+++ traits-and-currying/src/main/scala/CurryingExamples.scala
@@ -11,11 +11,6 @@ object CurryingExamples {
   def funGen[T](a: T, b: Some[T]) = (a,b)
   def funGenCurried[T](a: T)(b: Some[T]) = (a,b)
 
-  def modN(n: Int)(x: Int) = ((x % n) == 0)
-  def modNnc(n: Int, x: Int) = ((x % n) == 0)
-  def filterByMod2(l:Seq[Int]) = l.filter(modN(2))
-  def filterByMod2nc(l:Seq[Int]) = l.filter(modNnc(2,_))
-
   def main(args: Array[String]): Unit = {
     val x: (Any, Some[Any]) = funGen(1, Some("A")) //type inference cannot help here
     //val y = funGenCurried(1)(Some("A")) // correctly shows a compile error
--- traits-and-currying/src/test/scala/CurryingTests.scala
+++ traits-and-currying/src/test/scala/CurryingTests.scala
@@ -1,9 +1,16 @@
 import org.scalatest._
 
 class CurryingTests extends FlatSpec with MustMatchers {
-  "Test" should "have tests" in {
-    CurryingExamples.filterByMod2(1 to 10) must be === 2 :: 4 :: 6 :: 8 :: 10 :: Nil
-    CurryingExamples.filterByMod2nc(1 to 10) must be === 2 :: 4 :: 6 :: 8 :: 10 :: Nil
 
+  "filterByMod2" should "should filter by modulo 2 using the curried modN function" in {
+    def modN(n: Int)(x: Int) = ((x % n) == 0)
+    def filterByMod2(numbers: Seq[Int]): Seq[Int] = ???
+    filterByMod2(1 to 10) must be === 2 :: 4 :: 6 :: 8 :: 10 :: Nil
+  }
+
+  "filterByMod2notCurried" should "should filter by modulo 2 using the non-curried modN function" in {
+    //def modNnotCurried(n: Int, x: Int) = ((x % n) == 0)
+    def filterByMod2notCurried(numbers: Seq[Int]): Seq[Int] = ???
+    filterByMod2notCurried(1 to 10) must be === 2 :: 4 :: 6 :: 8 :: 10 :: Nil
   }
 }
