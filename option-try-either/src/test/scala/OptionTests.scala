a45e25064df2e25a7169f97a434d2314b5bedbe0
Susie Marshall <smarshal@thoughtworks.com>
2015-04-01 12:14:24 +0200

    Susie - slight modification to the test descriptions

--- option-try-either/src/test/scala/OptionTests.scala
+++ option-try-either/src/test/scala/OptionTests.scala
@@ -4,27 +4,27 @@ class OptionTests extends FlatSpec with MustMatchers {
   val aList = List(1,2,3,4,5)
   val anOption = Some(3)
 
-  "it" should "return a list of list of A" in {
+  "firstListFunction" should "return a list of list of A" in {
     OptionExamples.firstListFunction(aList) must be (List(List(2), List(3), List(4), List(5), List(6)))
   }
 
-  "it" should "return a list of A" in {
+  "secondListFunction" should "return a list of A" in {
     OptionExamples.secondListFunction(aList) must be (List(2,3,4,5,6))
   }
 
-  "it" should "return a some of some of A" in {
+  "firstOptionFunction" should "return a some of some of A" in {
     OptionExamples.firstOptionFunction(anOption) must be (Some(Some(4)))
   }
 
-  "it" should "return a some of A" in {
+  "secondOptionFunction" should "return a some of A" in {
     OptionExamples.secondOptionFunction(anOption) must be (Some(4))
   }
 
-  "it" should "work with None" in {
+  "firstOptionFunction" should "work with None" in {
     OptionExamples.firstOptionFunction(None) must be (Some(None))
   }
 
-  "it" should "also work with None" in {
+  "secondOptionFunction" should "also work with None" in {
     OptionExamples.secondOptionFunction(None) must be (None)
   }
 
