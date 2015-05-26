e4bf694955f4af3469942314f5eab786a01fdb46
Arif Wider <awider@thoughtworks.com>
2015-05-26 13:31:03 +0200

    added test for simple extractor exercise

--- pattern-matching/src/main/scala/PatternMatching.scala
+++ pattern-matching/src/main/scala/PatternMatching.scala
@@ -16,14 +16,6 @@ object PatternMatching {
 
   class PremiumUser(val name: String, val score: Int) extends User
 
-  object FreeUser {
-    def unapply(user: FreeUser): Option[(String, Int, Double)] =
-      Some((user.name, user.score, user.upgradeProbability))
-  }
-
-  object PremiumUser {
-    def unapply(user: PremiumUser): Option[(String, Int)] = Some((user.name, user.score))
-  }
 
   def personalizedGreeting(user: User): String = ???
 }
--- pattern-matching/src/test/scala/PatternMatchingTests.scala
+++ pattern-matching/src/test/scala/PatternMatchingTests.scala
@@ -1,14 +1,24 @@
-import PatternMatching.{PremiumUser, FreeUser, User}
+import PatternMatching._
 import org.scalatest._
 
 class PatternMatchingTests extends FlatSpec with MustMatchers {
-  "A personlized greeting" should "be returned for a user" in {
+  "Personlized greeting" should "work with user case classes" in {
+    val freeUser: User = new FreeUserCC("Tom", 500, 0.1d)
+    val potentialPremiumUser: User = new FreeUserCC("John", 1000, 0.8d)
+    val premiumUser: User = new PremiumUserCC("Brian", 3000)
+
+    personalizedGreeting(freeUser) must === ("Hello Tom")
+    personalizedGreeting(potentialPremiumUser) must === ("John, what can we do for you today?")
+    personalizedGreeting(premiumUser) must === ("Welcome back, dear Brian")
+  }
+
+  "Personlized greeting" should "work with user non-case classes" in {
     val freeUser: User = new FreeUser("Tom", 500, 0.1d)
     val potentialPremiumUser: User = new FreeUser("John", 1000, 0.8d)
     val premiumUser: User = new PremiumUser("Brian", 3000)
 
-    PatternMatching.personalizedGreeting(freeUser) must === ("Hello Tom")
-    PatternMatching.personalizedGreeting(potentialPremiumUser) must === ("John, what can we do for you today?")
-    PatternMatching.personalizedGreeting(premiumUser) must === ("Welcome back, dear Brian")
+    personalizedGreeting(freeUser) must === ("Hello Tom")
+    personalizedGreeting(potentialPremiumUser) must === ("John, what can we do for you today?")
+    personalizedGreeting(premiumUser) must === ("Welcome back, dear Brian")
   }
 }
