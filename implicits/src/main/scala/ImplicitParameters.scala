84cb3922767fff2bcc098d3248ad60e93657bba2
Susie Marshall <smarshal@thoughtworks.com>
2015-06-09 12:31:30 +0200

    Removing the 's' from Implicit

--- implicits/src/main/scala/ImplicitParameters.scala
+++ implicits/src/main/scala/ImplicitParameters.scala
@@ -2,7 +2,7 @@ object MathConstants {
   implicit val PI = 3.1415901244
 }
 
-object ImplicitsParameters {
+object ImplicitParameters {
 
   // TODO: Exercise 2 - make the test pass without importing Math.Constants here
   def calculateCircumference(radius: Double): Double = ???
