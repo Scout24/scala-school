eab39330fa6c81f298e19e3ce17cbba1c79119ba
Arif Wider <awider@thoughtworks.com>
2015-06-29 16:22:01 +0200

    completed model to json conversion example

--- play-json/app/models/Vehicle.scala
+++ play-json/app/models/Vehicle.scala
@@ -3,6 +3,11 @@ package models
 import play.api.libs.json._
 
 case class Address(street: String, no: Int, zip: Int)
+
+object Address {
+  implicit val addressFormat: Format[Address] = Json.format[Address]
+}
+
 case class Vehicle(id: Int, make: String, model: String, registAdd: Address)
 
 object Vehicle {
@@ -12,12 +17,13 @@ object Vehicle {
     Vehicle(2, "Audi", "A4", Address("Dingolfinger Str", 9, 81673))
   )
 
-  implicit object VehicleWrites extends Writes[Vehicle] {
+  implicit val vehicleWrites = new Writes[Vehicle] {
+    import Address._
     def writes(v: Vehicle) = JsObject(Seq(
       ("id", JsNumber(v.id)),
       ("make", JsString(v.make)),
       ("model", JsString(v.model)),
-      ("registAdd", JsString(v.registAdd.toString))
+      ("registAdd", Json.toJson(v.registAdd))
     ))
   }
 }
