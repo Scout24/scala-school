96905bce2db50b3d66ccee0beeea8f5236ca322e
Arif Wider <awider@thoughtworks.com>
2015-08-09 23:49:08 +0200

    changing examples to focus on option only: remove solutions

--- option-try-either/build.sbt
+++ option-try-either/build.sbt
@@ -1,4 +1,4 @@
-name := """option-try-either"""
+name := """option"""
 
 version := "1.0"
 
--- option-try-either/src/main/scala/OptionExamples.scala
+++ option-try-either/src/main/scala/OptionExamples.scala
@@ -4,27 +4,15 @@ object OptionExamples {
   val incrementWrapOptionFunction = { a : Int => Some(a + 1) }
   val filterFunction = { a : Int => a > 2 }
 
-  def firstListFunction(aList: List[Int]): List[List[Int]] = {
-    aList.map(incrementWrapListFunction)
-  }
+  def firstListFunction(aList: List[Int]): List[List[Int]] = ???
   
-  def secondListFunction(aList: List[Int]): List[Int] = {
-    aList.flatMap(incrementWrapListFunction)
-  }
+  def secondListFunction(aList: List[Int]): List[Int] = ???
   
-  def thirdListFunction(aList: List[Int]): List[Int] = {
-    aList.filter(filterFunction)
-  }
+  def thirdListFunction(aList: List[Int]): List[Int] = ???
 
-  def firstOptionFunction(option: Option[Int]): Option[Option[Int]] = {
-    option.map(incrementWrapOptionFunction)
-  }
+  def firstOptionFunction(option: Option[Int]): Option[Option[Int]] = ???
 
-  def secondOptionFunction(option: Option[Int]): Option[Int] = {
-    option.flatMap(incrementWrapOptionFunction)
-  }
+  def secondOptionFunction(option: Option[Int]): Option[Int] = ???
 
-  def thirdOptionFunction(option: Option[Int]): Option[Int] = {
-    option.filter(filterFunction)
-  }
+  def thirdOptionFunction(option: Option[Int]): Option[Int] = ???
 }
