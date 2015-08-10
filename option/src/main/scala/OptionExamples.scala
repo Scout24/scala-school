28e2f19000f2db7b8ddefcb53fc07b09d8d86412
Arif Wider <awider@thoughtworks.com>
2015-08-10 15:59:20 +0200

    Susie/Arif changing exercises to focus on option only: better method names

--- option/src/main/scala/OptionExamples.scala
+++ option/src/main/scala/OptionExamples.scala
@@ -4,15 +4,15 @@ object OptionExamples {
   val incrementWrapOptionFunction = { a : Int => Some(a + 1) }
   val filterFunction = { a : Int => a > 2 }
 
-  def firstListFunction(aList: List[Int]): List[List[Int]] = ???
+  def incrementListFunction(aList: List[Int]): List[List[Int]] = ???
   
-  def secondListFunction(aList: List[Int]): List[Int] = ???
+  def incrementAndFlattenListFunction(aList: List[Int]): List[Int] = ???
   
-  def thirdListFunction(aList: List[Int]): List[Int] = ???
+  def filterListFunction(aList: List[Int]): List[Int] = ???
 
-  def firstOptionFunction(option: Option[Int]): Option[Option[Int]] = ???
+  def incrementOptionFunction(option: Option[Int]): Option[Option[Int]] = ???
 
-  def secondOptionFunction(option: Option[Int]): Option[Int] = ???
+  def incrementAndFlattenOptionFunction(option: Option[Int]): Option[Int] = ???
 
-  def thirdOptionFunction(option: Option[Int]): Option[Int] = ???
+  def filterOptionFunction(option: Option[Int]): Option[Int] = ???
 }
