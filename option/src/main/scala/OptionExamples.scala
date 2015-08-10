63179a2d3d679b2e993a89271c6bd9cfd516477e
Susie Marshall <smarshal@thoughtworks.com>
2015-08-10 16:27:18 +0200

    Susie changed the function names

--- option/src/main/scala/OptionExamples.scala
+++ option/src/main/scala/OptionExamples.scala
@@ -4,15 +4,15 @@ object OptionExamples {
   val OptionFunction = { a : Int => Some(a + 1) }
   val filterFunction = { a : Int => a > 2 }
 
-  def incrementListFunction(aList: List[Int]): List[List[Int]] = ???
+  def incrementList(aList: List[Int]): List[List[Int]] = ???
   
-  def incrementAndFlattenListFunction(aList: List[Int]): List[Int] = ???
+  def incrementAndFlattenList(aList: List[Int]): List[Int] = ???
   
-  def filterListFunction(aList: List[Int]): List[Int] = ???
+  def filterList(aList: List[Int]): List[Int] = ???
 
-  def incrementOptionFunction(option: Option[Int]): Option[Option[Int]] = ???
+  def incrementOption(option: Option[Int]): Option[Option[Int]] = ???
 
-  def incrementAndFlattenOptionFunction(option: Option[Int]): Option[Int] = ???
+  def incrementAndFlattenOption(option: Option[Int]): Option[Int] = ???
 
-  def filterOptionFunction(option: Option[Int]): Option[Int] = ???
+  def filterOption(option: Option[Int]): Option[Int] = ???
 }
--- option/src/test/scala/OptionTests.scala
+++ option/src/test/scala/OptionTests.scala
@@ -5,37 +5,37 @@ class OptionTests extends FlatSpec with MustMatchers {
   val anOption = Some(3)
 
   "firstListFunction" should "return a list of list of A" in {
-    OptionExamples.incrementListFunction(aList) must be (List(List(2), List(3), List(4), List(5), List(6)))
+    OptionExamples.incrementList(aList) must be (List(List(2), List(3), List(4), List(5), List(6)))
   }
 
   "secondListFunction" should "return a list of A" in {
-    OptionExamples.incrementAndFlattenListFunction(aList) must be (List(2,3,4,5,6))
+    OptionExamples.incrementAndFlattenList(aList) must be (List(2,3,4,5,6))
   }
 
   "thirdListFunction" should "return a list of A" in {
-    OptionExamples.filterListFunction(aList) must be (List(3,4,5))
+    OptionExamples.filterList(aList) must be (List(3,4,5))
   }
 
   "firstOptionFunction" should "return a some of some of A" in {
-    OptionExamples.incrementOptionFunction(anOption) must be (Some(Some(4)))
+    OptionExamples.incrementOption(anOption) must be (Some(Some(4)))
   }
 
   "secondOptionFunction" should "return a some of A" in {
-    OptionExamples.incrementAndFlattenOptionFunction(anOption) must be (Some(4))
+    OptionExamples.incrementAndFlattenOption(anOption) must be (Some(4))
   }
 
   "firstOptionFunction" should "work with None" in {
-    OptionExamples.incrementOptionFunction(None) must be (None)
+    OptionExamples.incrementOption(None) must be (None)
   }
 
   "secondOptionFunction" should "also work with None" in {
-    OptionExamples.incrementAndFlattenOptionFunction(None) must be (None)
+    OptionExamples.incrementAndFlattenOption(None) must be (None)
   }
 
   "thirdOptionFunction" should "filter" in {
-    OptionExamples.filterOptionFunction(Some(3)) must be (Some(3))
-    OptionExamples.filterOptionFunction(Some(2)) must be (None)
-    OptionExamples.filterOptionFunction(None) must be (None)
+    OptionExamples.filterOption(Some(3)) must be (Some(3))
+    OptionExamples.filterOption(Some(2)) must be (None)
+    OptionExamples.filterOption(None) must be (None)
   }
 
 
