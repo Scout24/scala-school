9113fb169ede0be7a2cbbe42057ecfcd1b1952fc
Chris Wewerka <breadfan@gmx.de>
2015-09-14 15:25:50 +0200

    first example now uses lang try to thow the burden, improved doc

--- failure-handling/src/main/scala/customer/CustomersListView.scala
+++ failure-handling/src/main/scala/customer/CustomersListView.scala
@@ -8,19 +8,21 @@ class CustomersListView(customerService: CustomerService) {
 
   /**
    * Use [[customerService.customerName(id)]] for implementing a Seq of existing customer names for the given customer ids.
-   * I.e. filter out all ids for that the Repo throws it's exceptions.
+   * I.e. remove things for that the Repo throws it's customer not found exception.
    *
-   * You may freely use try or Try or another method for handling the exceptions of the underlying repo method.
+   * You should try to use the scala language try/catch here (not the scala library Try class) for handling the exceptions of the underlying repo method.
    *
-   * Hint: If you use Try() have a look at collect method in http://www.scala-lang.org/api/current/#scala.collection.Seq in conjunction with pattern matching.
-   * Remember with pattern matching you may define a partial function!
-   *
-   * Do you see the burden that is put on the client when using Exceptions for "business return values"?
+   * Do you feel the burden that is put on the client when using Exceptions for "business return values"?
+   * Code using exceptions and try/catch language construct usually feels less functional.
    */
   def listExistingCustomerNamesLangTryBased(ids: Seq[Int]): Seq[String] = {
-    val possibleCustomerNames = ids.map { id => Try(customerService.customerName(id)) }
-    possibleCustomerNames.collect {
-      case Success(name) => name
+    ids.flatMap {
+      customerId =>
+        try {
+          Some(customerService.customerName(customerId))
+        } catch {
+          case e: CustomerNotFoundException => None
+        }
     }
   }
 
@@ -29,6 +31,9 @@ class CustomersListView(customerService: CustomerService) {
    *
    * Hint: Have a look at collect method in http://www.scala-lang.org/api/current/#scala.collection.Seq in conjunction with pattern matching.
    * Remember with pattern matching you may define a partial function!
+   *
+   * The scala library Try is usually a good fit to integrate code/libraries that uses exceptions in a functional manner.
+   * See http://www.scala-lang.org/api/current/#scala.util.Try for great examples on Try.
    */
   def listExistingCustomerNamesTryBased(ids: Seq[Int]): Seq[String] = {
     ids.map(customerService.customerNameTry).collect {
