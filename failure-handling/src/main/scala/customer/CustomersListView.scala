f32cf7788c792ecf364f7a81c4635ae0f6a51de0
Chris Wewerka <breadfan@gmx.de>
2015-09-14 15:13:43 +0200

    added links to gist

--- failure-handling/src/main/scala/customer/CustomersListView.scala
+++ failure-handling/src/main/scala/customer/CustomersListView.scala
@@ -8,14 +8,14 @@ class CustomersListView(customerService: CustomerService) {
 
   /**
    * Use [[customerService.customerName(id)]] for implementing a Seq of existing customer names for the given customer ids.
-   * I.e. filter out all ids for that the Repo throws it's exceptions.
+   * I.e. filter out all ids for that the Repo throws customer not found exceptions.
    *
    * You may freely use try or Try or another method for handling the exceptions of the underlying repo method.
    *
    * Hint: If you use Try() have a look at collect method in http://www.scala-lang.org/api/current/#scala.collection.Seq in conjunction with pattern matching.
    * Remember with pattern matching you may define a partial function!
    *
-   * TODO gist for collect
+   * See gist for a collect() demo: https://gist.github.com/breadfan/0a422f009ad17e2c8283
    *
    * Do you see the burden that is put on the client when using Exceptions for "business return values"?
    */
@@ -27,16 +27,14 @@ class CustomersListView(customerService: CustomerService) {
    * Hint: Have a look at collect method in http://www.scala-lang.org/api/current/#scala.collection.Seq in conjunction with pattern matching.
    * Remember with pattern matching you may define a partial function!
    *
-   * TODO gist for collect
+   * See gist for a collect() demo: https://gist.github.com/breadfan/0a422f009ad17e2c8283
    */
   def listExistingCustomerNamesTryBased(ids: Seq[Int]): Seq[String] = ???
 
   /**
    * Same as above but now use [[customerService.customerNameOption(id)]].
    *
-   * Hint: Have a look at map() and flatten() on Seq to solve this problem very concisely. Is there a way to be even more concise?
-   *
-   * TODO gist for flatten on List[Option]
+   * Hint: Have a look at flatten() on Seq/List to solve this problem very concisely.
    */
   def listExistingCustomerNamesOptionBased(ids: Seq[Int]): Seq[String] = ???
 
@@ -46,7 +44,7 @@ class CustomersListView(customerService: CustomerService) {
    * Hint: Have a look at collect method in http://www.scala-lang.org/api/current/#scala.collection.Seq in conjunction with pattern matching.
    * Remember with pattern matching you may define a partial function!
    *
-   * TODO gist for collect
+   * See gist for a collect() demo: https://gist.github.com/breadfan/0a422f009ad17e2c8283
    */
   def listExistingCustomerNamesScalacticOrBased(ids: Seq[Int]): Seq[String] = ???
 }
