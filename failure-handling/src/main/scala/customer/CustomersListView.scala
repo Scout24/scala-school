5ee6c7ab948cead7d54077c5637b469c7ef7f986
Chris Wewerka <breadfan@gmx.de>
2015-09-14 15:30:21 +0200

    forcing to use lang try for first example

--- failure-handling/src/main/scala/customer/CustomersListView.scala
+++ failure-handling/src/main/scala/customer/CustomersListView.scala
@@ -8,16 +8,12 @@ class CustomersListView(customerService: CustomerService) {
 
   /**
    * Use [[customerService.customerName(id)]] for implementing a Seq of existing customer names for the given customer ids.
-   * I.e. filter out all ids for that the Repo throws customer not found exceptions.
+   * I.e. remove things for that the Repo throws it's customer not found exception.
    *
-   * You may freely use try or Try or another method for handling the exceptions of the underlying repo method.
+   * You should try to use the scala language try/catch here (not the scala library Try class) for handling the exceptions of the underlying repo method.
    *
-   * Hint: If you use Try() have a look at collect method in http://www.scala-lang.org/api/current/#scala.collection.Seq in conjunction with pattern matching.
-   * Remember with pattern matching you may define a partial function!
-   *
-   * See gist for a collect() demo: https://gist.github.com/breadfan/0a422f009ad17e2c8283
-   *
-   * Do you see the burden that is put on the client when using Exceptions for "business return values"?
+   * Do you feel the burden that is put on the client when using Exceptions for "business return values"?
+   * Code using exceptions and try/catch language construct usually feels less functional.
    */
   def listExistingCustomerNamesLangTryBased(ids: Seq[Int]): Seq[String] = ???
 
@@ -34,7 +30,7 @@ class CustomersListView(customerService: CustomerService) {
   /**
    * Same as above but now use [[customerService.customerNameOption(id)]].
    *
-   * Hint: Have a look at flatten() on Seq/List to solve this problem very concisely.
+   * Hint: Have a look at map() and flatten() on Seq to solve this problem very concisely. Is there a way to be even more concise, i.e. combine map and flatten?
    */
   def listExistingCustomerNamesOptionBased(ids: Seq[Int]): Seq[String] = ???
 
