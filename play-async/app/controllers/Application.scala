60ba76c9f01d0b8c6b231dc4f0a43ec76313be81
Arif Wider <awider@thoughtworks.com>
2015-10-11 22:17:11 +0200

    removed for comprehension solution for exercise 2

--- play-async/app/controllers/Application.scala
+++ play-async/app/controllers/Application.scala
@@ -2,7 +2,6 @@ package controllers
 
 import javax.inject.Inject
 
-import models.{LocalModelsCounterMoto, LocalModelsCounterCars}
 import play.api.libs.ws.{WSResponse, WSClient}
 import play.api.mvc._
 import views.html.{index}
@@ -12,23 +11,22 @@ import scala.concurrent.Future
 
 class Application @Inject()(ws: WSClient) extends Controller {
 
-  // TODO Exercise 2 - switch to remote counter source
   def root = Action {
+    import models.{LocalModelsCounterMoto, LocalModelsCounterCars}
     val counter = LocalModelsCounterCars.counter + LocalModelsCounterMoto.counter
     Ok(index(counter))
   }
 
   case class CounterResponse(counter: Int)
   implicit val crf = Json.format[CounterResponse]
+  def counterFromJsonResponse(response: WSResponse) = response.json.validate[CounterResponse].get.counter
 
+  // TODO Exercise 2 - implement same behaviour as above but using remote counter sources
   def rootremote = Action.async {
     import scala.concurrent.ExecutionContext.Implicits.global
-    val url = "http://localhost:9000"
-    val carsCounter = ws.url(url + "/carscounter").get
-    val motoCounter = ws.url(url + "/motocounter").get
-    for {
-      cc <- carsCounter.map(_.json.validate[CounterResponse].get.counter)
-      mc <- motoCounter.map(_.json.validate[CounterResponse].get.counter)
-    } yield Ok(index(cc + mc))
+    val carsCounter = ws.url("http://localhost:9000/carscounter").get
+    val motoCounter = ws.url("http://localhost:9000/motocounter").get
+    val res: Future[Result] = ???
+    res
   }
 }
