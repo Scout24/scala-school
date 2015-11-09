7bf4d2c41642f8a1b1a81a58f470c4c8351685a9
Christian Treppo <christian@treppo.org>
2015-11-09 17:32:19 +0100

    Add configuration for the Thing

--- play-2_3/app/app/Thing.scala
+++ play-2_3/app/app/Thing.scala
@@ -1,12 +1,17 @@
 package app
 
+import play.api.Play
+import play.api.Play.current
+
 object Thing {
 
+  val name = Play.configuration.underlying.getString("thing.name")
+
   def start(): Unit = {
-    println("Start up thing")
+    println(s"Start up $name")
   }
 
   def stop(): Unit = {
-    println("Stop the thing")
+    println(s"Stop the $name")
   }
 }
