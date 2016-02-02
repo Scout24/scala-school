894933e1ec91578b33e7289e0047c996baeb81e8
matlloyd <matlloyd@thoughtworks.com>
2016-02-02 16:49:37 +0100

    Matt: reverting the Tennis Game 1 back to its original

--- tennis-kata-1/src/main/scala/tennis/TennisGame1.scala
+++ tennis-kata-1/src/main/scala/tennis/TennisGame1.scala
@@ -1,59 +1,54 @@
 package tennis
 
 
-class TennisGame1(val player1Name: String, val player2Name: String) extends TennisGame {
-  // TODO:
+class TennisGame1 (val player1Name : String, val player2Name : String) extends TennisGame {
   var m_score1: Int = 0
   var m_score2: Int = 0
 
-  def wonPoint(player: String) {
-    if (player == "player1")
-      m_score1 += 1
-    else
-      m_score2 += 1
-  }
-
-  def mapScoreToString(score: Int): String =
-    m_score1 match {
-      case 0 => "Love-All"
-      case 1 => "Fifteen-All"
-      case 2 => "Thirty-All"
-      case _ => "Deuce"
-    }
-
-
-  def scoreOf(score1: Int): String =
-    score1 match {
-      case 0 => "Love"
-      case 1 => "Fifteen"
-      case 2 => "Thirty"
-      case 3 => "Forty"
-    }
-
-  //what is expected here
-
-  def getScore(): String = s"${scoreOf(m_score1)}-${scoreOf(m_score2)}"
-
-  def calculateScore(): String = {
-    // Only when the scores are equal.
-    if (m_score1 == m_score2) {
-      // map the score to something else
-      mapScoreToString(m_score1)
-    }
-    // if either score is above 4
-    else if (m_score1 >= 4 || m_score2 >= 4) {
-
-      val minusResult = m_score1 - m_score2
-      minusResult match {
-        case 1 => "Advantage player1"
-        case -1 => "Advantage player2"
-        case score if score >= 2 => "Win for player1"
-        case score => "Win for player2"
+  def wonPoint(playerName : String) {
+          if (playerName == "player1")
+              m_score1 += 1
+          else
+              m_score2 += 1
       }
-    }
-    else {
-      getScore()
-    }
-  }
-}
 
+  def calculateScore() : String = {
+      var score : String = ""
+      var tempScore=0
+      if (m_score1==m_score2)
+      {
+        score = m_score1 match {
+              case 0 =>  "Love-All"
+              case 1 => "Fifteen-All"
+              case 2 => "Thirty-All"
+              case _ => "Deuce"
+
+          }
+      }
+      else if (m_score1>=4 || m_score2>=4)
+      {
+          val minusResult = m_score1-m_score2
+          if (minusResult==1) score ="Advantage player1"
+          else if (minusResult == -1) score ="Advantage player2"
+          else if (minusResult>=2) score = "Win for player1"
+          else score ="Win for player2"
+      }
+      else
+      {
+          for ( i<- 1 until 3 by 1)
+          {
+              if (i==1) tempScore = m_score1
+              else { score+="-"; tempScore = m_score2;}
+              val tempScore2 = tempScore match {
+                  case 0 => "Love"
+                  case 1 => "Fifteen"
+                  case 2 => "Thirty"
+                  case 3 => "Forty"
+              }
+            score += tempScore2
+          }
+      }
+    return score
+  }
+
+}
