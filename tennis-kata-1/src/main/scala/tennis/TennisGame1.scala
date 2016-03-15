eeaea7782ea4fdf7c98712271319db69c5272916
matlloyd <matlloyd@thoughtworks.com>
2016-03-15 10:38:35 +0100

    Matt: Adding the previous Dojo as a branch

--- tennis-kata-1/src/main/scala/tennis/TennisGame1.scala
+++ tennis-kata-1/src/main/scala/tennis/TennisGame1.scala
@@ -1,54 +1,56 @@
 package tennis
 
 
-class TennisGame1 (val player1Name : String, val player2Name : String) extends TennisGame {
-  var m_score1: Int = 0
-  var m_score2: Int = 0
+class TennisGame1(val player1Name: String, val player2Name: String) extends TennisGame {
 
-  def wonPoint(playerName : String) {
-          if (playerName == "player1")
-              m_score1 += 1
-          else
-              m_score2 += 1
-      }
+  // This is ugly!
+  var player1Score: Int = 0
+  var player2Score: Int = 0
 
-  def calculateScore() : String = {
-      var score : String = ""
-      var tempScore=0
-      if (m_score1==m_score2)
-      {
-        score = m_score1 match {
-              case 0 =>  "Love-All"
-              case 1 => "Fifteen-All"
-              case 2 => "Thirty-All"
-              case _ => "Deuce"
-
-          }
-      }
-      else if (m_score1>=4 || m_score2>=4)
-      {
-          val minusResult = m_score1-m_score2
-          if (minusResult==1) score ="Advantage player1"
-          else if (minusResult == -1) score ="Advantage player2"
-          else if (minusResult>=2) score = "Win for player1"
-          else score ="Win for player2"
-      }
-      else
-      {
-          for ( i<- 1 until 3 by 1)
-          {
-              if (i==1) tempScore = m_score1
-              else { score+="-"; tempScore = m_score2;}
-              val tempScore2 = tempScore match {
-                  case 0 => "Love"
-                  case 1 => "Fifteen"
-                  case 2 => "Thirty"
-                  case 3 => "Forty"
-              }
-            score += tempScore2
-          }
-      }
-    return score
+  // Mutates the internal state.
+  def wonPoint(playerName: String) {
+    if (playerName == player1Name)
+      player1Score += 1
+    else if (playerName == player2Name)
+      player2Score += 1
+    else
+      throw new Exception("He's not playing!!")
   }
 
+  def calculateScore(): String = {
+
+    // if scores are equal?
+    if (player1Score == player2Score) {
+      player1Score match {
+        case 0 => "Love-All"
+        case 1 => "Fifteen-All"
+        case 2 => "Thirty-All"
+        case _ => "Deuce"
+      }
+    }
+    // possible win
+
+    else if (player1Score >= 4 || player2Score >= 4) {
+      player1Score - player2Score match {
+        case 0 if player1Score < 3 => convertScoreToString(player1Score) + "-All"
+        case 0 =>  "Deuce"
+        case 1 => "Advantage player1"
+        case -1 => "Advantage player2"
+        case d if d >= 2 => "Win for player1"
+        case _ => "Win for player2"
+      }
+    }
+    else {
+      convertScoreToString(player1Score) + "-" + convertScoreToString(player2Score)
+    }
+  }
+
+  def convertScoreToString(score: Int): String = {
+    score match {
+      case 0 => "Love"
+      case 1 => "Fifteen"
+      case 2 => "Thirty"
+      case 3 => "Forty"
+    }
+  }
 }
